f318eeb368df3ba3d79379e162831130a6ad09f2
achuchuchu <90691146+achuchuchu-dayo@users.noreply.github.com>
2021-12-02 14:23:49 +0900

    learned how to limit the blog nums

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by("-date")[:3]
     return render(request, 'blog/blog.html', {'blogs':blogs})
 # Create your views here.
