83704492e0fbbcaf2d6005a4c73e38ac5279f2eb
achuchuchu <90691146+achuchuchu-dayo@users.noreply.github.com>
2021-12-02 14:11:43 +0900

    grasp how the DB works

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/blog.html')
+    blogs = Blog.objects.all()
+    return render(request, 'blog/blog.html', {'blogs':blogs})
 # Create your views here.
